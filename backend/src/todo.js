46bd6063876f581c43fcce266f1645ade1161793
Gubala Haribabu Laasya Sai <lagu23@student.bth.se>
2025-03-14 18:20:31 +0100

    Added Add and Delete Funcs

---
+++ backend/src/todo.js
@@ -0,0 +1,14 @@
+let todos = [];
+
+function addTodo(todos, newTodo) {
+    return [...todos, newTodo];
+}
+function deleteTodo(todos, id) {
+    return todos.filter(todo => todo.id !== id);
+}
+
+
+module.exports = { 
+    addTodo, 
+    deleteTodo,  
+};
